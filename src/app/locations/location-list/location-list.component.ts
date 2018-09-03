42db033a65c99c555eb53c3b00eaa4291b61c613
Vai <vaidorass@live.com>
2018-09-03 11:01:41 -0500

    initialized location module and component

---
+++ src/app/locations/location-list/location-list.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from "@angular/core";
+
+import {} from "app/shared/Services/"
+
+@Component({
+  selector: "app-location-list",
+  templateUrl: "./location-list.component.html",
+  styles: []
+})
+export class LocationListComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
---
+++ src/app/locations/locations.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LocationListComponent } from './location-list/location-list.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [LocationListComponent]
+})
+export class LocationsModule { }
